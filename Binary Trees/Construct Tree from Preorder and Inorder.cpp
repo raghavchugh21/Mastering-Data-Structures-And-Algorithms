ae5ba5f8b615146296ae06df44087e0c95f16308
raghavchugh21 <65908705+raghavchugh21@users.noreply.github.com>
2021-01-13 14:31:12 +0530

    Binary Trees Day 6

---
+++ Binary Trees/Construct Tree from Preorder and Inorder.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+using namespace std;
+#include "solution.h"
+
+BinaryTreeNode<int> *takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+        
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int> *rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+void printLevelATNewLine(BinaryTreeNode<int> *root) {
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *first = q.front();
+        q.pop();
+        if (first == NULL) {
+            if (q.empty()) {
+                break;
+            }
+            cout << endl;
+            q.push(NULL);
+            continue;
+        }
+        cout << first->data << " ";
+        if (first->left != NULL) {
+            q.push(first->left);
+        }
+        if (first->right != NULL) {
+            q.push(first->right);
+        }
+    }
+}
+BinaryTreeNode<int>* buildTree(int *preorder, int preLength, int *inorder, int inLength) {
+    
+    if(inLength<=0||preLength<=0)
+        return NULL;
+    
+    BinaryTreeNode<int>*root = new BinaryTreeNode<int>(preorder[0]);
+    
+    int rootIndex;
+    
+    for(rootIndex=0;inorder[rootIndex]!=preorder[0];rootIndex++);
+
+    root->left = buildTree(preorder+1,rootIndex,inorder,rootIndex);
+    root->right = buildTree(preorder+rootIndex+1,preLength-rootIndex-1,inorder+rootIndex+1,inLength-rootIndex-1);
+    
+    return root;
+
+}
+
+int main() {
+    int size;
+    cin >> size;
+    int *pre = new int[size];
+    int *in = new int[size];
+    for (int i = 0; i < size; i++) cin >> pre[i];
+    for (int i = 0; i < size; i++) cin >> in[i];
+    BinaryTreeNode<int> *root = buildTree(pre, size, in, size);
+    printLevelATNewLine(root);
+}
---
+++ Binary Trees/Min and Max of Binary Tree.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+using namespace std;
+#include "solution.h"
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+#include<bits/stdc++.h>
+pair<int, int> getMinAndMax(BinaryTreeNode<int> *root) {
+    if(root==NULL){
+        pair<int,int> p(INT_MAX,INT_MIN);
+        return p;
+    }
+    
+    pair<int,int> l = getMinAndMax(root->left);
+    pair<int,int> r = getMinAndMax(root->right);
+    
+    pair<int,int> ans (min(root->data,min(l.first,r.first)),max(root->data,max(l.second,r.second)));
+    return ans;
+}
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    pair<int, int> ans = getMinAndMax(root);
+    cout << ans.first << " " << ans.second;
+}
